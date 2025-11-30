b5e8b9c58fb6ca04647f0bdd3b73f165a7f3ae3e
lanzslides <lfermin.connect@gmail.com>
2025-12-01 02:35:46 +0800

    added HouseholdAddForm

--- fiesviewer/calabarzonapp/forms.py
+++ fiesviewer/calabarzonapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Province
+from .models import Province, Household
+from django.db.models import Max
 
 AREA_CHOICES = (
     ('', 'Any'),
@@ -43,3 +44,36 @@ class HouseholdFilterForm(forms.Form):
         min_value=0,
         widget=forms.NumberInput(attrs={'step': 10000, 'min': 0})
     )
+
+class HouseholdAddForm(forms.ModelForm):
+    class Meta:
+        model = Household
+        fields = [
+            'province', 'FSIZE', 'URB', 'RFACT', 'TOINC', 'WAGES',
+            'RPCINC', 'CASH_ABROAD', 'CASH_DOMESTIC', 'TOTEX', 'PERCAPITA',
+            'FOOD', 'CLOTH', 'HEALTH', 'TRANSPORT', 'COMMUNICATION',
+            'RECREATION', 'EDUCATION'
+        ]
+
+        # Add attrs={'class': 'form-control'} later
+        # Are there assumptions about TOINC and TOTEX?
+        widgets = {
+            'province': forms.Select(),
+            'FSIZE': forms.NumberInput(),
+            # 'URB': forms.NumberInput(), # changed to Select automatically because of choices
+            'RFACT': forms.NumberInput(), # is this needed
+            'TOINC': forms.NumberInput(), # this should be an autocomputed value
+            'WAGES': forms.NumberInput(),
+            'RPCINC': forms.NumberInput(), # is this needed
+            'CASH_ABROAD': forms.NumberInput(),
+            'CASH_DOMESTIC': forms.NumberInput(),
+            'TOTEX': forms.NumberInput(), # this should be an autocomputed value
+            'PERCAPITA': forms.NumberInput(), # is this needed
+            'FOOD': forms.NumberInput(),
+            'CLOTH': forms.NumberInput(),
+            'HEALTH': forms.NumberInput(),
+            'TRANSPORT': forms.NumberInput(),
+            'COMMUNICATION': forms.NumberInput(),
+            'RECREATION': forms.NumberInput(),
+            'EDUCATION': forms.NumberInput(),
+        }
