ff694074a628ead313b036e6bb57587396b825f4
Francis Velaque <velaque.francis@gmail.com>
2025-11-30 13:18:04 +0800

    Added all templates and navigations. Household list and household detail also done.

--- fiesviewer/calabarzonapp/urls.py
+++ fiesviewer/calabarzonapp/urls.py
@@ -3,6 +3,32 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    
+    # localhost:8000/fiesviewer/households
+    path('households/', views.listhouseholds, name='household-list'),
+
+    # localhost:8000/fiesviewer/households/<pk>
+    path('households/<int:pk>/', views.household_detail, name='household-detail'),
+
+    # localhost:8000/fiesviewer/households/form
+    path('households/form/', views.household_form, name='household-form'),
+
+    # localhost:8000/fiesviewer/households/edit # Add pk for the OIC
+    path('households/edit/', views.household_edit, name='household-edit'),
+
+    # localhost:8000/fiesviewer/households/delete # Add pk for the OIC
+    path('households/delete/', views.household_delete, name='household-delete'),
+
+    #localhost:8000/fiesviewer/households/search
+    path('households/search/', views.household_search, name='household-search'),
+
+    #localhost:8000/fiesviewer/households/filter
+    path('households/filter/', views.household_filter, name='household-filter'),
+
+    # localhost:8000/fiesviewer/province-level
+    path('province-level/', views.province_level, name='province-level'),
+
+
 
 ]
--- fiesviewer/calabarzonapp/views.py
+++ fiesviewer/calabarzonapp/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Household, Province
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome to the FIES Viewer!</h1>')
+    return render(request, 'calabarzonapp/base_template.html')
+
+def listhouseholds(request):
+    householdlist = Household.objects.all()
+    context = {
+        'householdlist': householdlist
+    }
+    return render(request, 'calabarzonapp/household_list.html', context)
+
+def household_detail(request, pk):
+    household = Household.objects.get(pk=pk)
+    context = {
+        'household': household
+    }
+    return render(request, 'calabarzonapp/household_detail.html', context)
+
+
+def household_form(request):
+    return render(request, 'calabarzonapp/household_form.html')
+
+def household_edit(request):
+    return render(request, 'calabarzonapp/household_form.html')
+
+def household_delete(request):
+    return render(request, 'calabarzonapp/household_delete.html')
+
+def household_search(request):
+    return render(request, 'calabarzonapp/household_search.html')
+
+def household_filter(request):
+    return render(request, 'calabarzonapp/household_filter.html')
+
+def province_level(request):
+    return render(request, 'calabarzonapp/province_level.html')
