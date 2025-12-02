31affae27d5bf33323789bc46b3f0234867b214f
RafaTagulao <rafael.tagulao@student.ateneo.edu>
2025-12-02 11:06:34 +0800

    added home and some design

--- fiesviewer/fiesviewer/urls.py
+++ fiesviewer/fiesviewer/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/fiesviewer/', permanent=False)),
     path('admin/', admin.site.urls),
     path('fiesviewer/', include('calabarzonapp.urls'))
 ]
